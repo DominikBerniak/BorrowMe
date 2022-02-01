0745d7a9c62b3e8ad0d6844d37685a43b69d3f64
Dominik Berniak <berniak.dominik@gmail.com>
2022-02-01 12:05:12 +0100

    projectFilesReact: Initialize react project, Configure router, Add pages: Home, Layout, PageNotFound, Add components: NavBar, Footer

---
+++ BorrowMeReact/src/App.js
@@ -0,0 +1,17 @@
+import {Route, Routes} from "react-router-dom";
+import Layout from "./pages/Layout";
+import PageNotFound from "./pages/PageNotFound";
+import Home from "./pages/Home";
+
+function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Layout/>}>
+                <Route index element={<Home/>}/>
+                <Route path="*" element={<PageNotFound/>}/>
+            </Route>
+        </Routes>
+    );
+}
+
+export default App;

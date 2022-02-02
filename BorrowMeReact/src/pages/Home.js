4fccaa2a77fe6a43785935355d373f6c3160f01a
Pawel Kaminski <kaminski.pawel404@gmail.com>
2022-02-02 14:19:17 +0100

    Front-end: Implement main page announcements

--- BorrowMeReact/src/pages/Home.js
+++ BorrowMeReact/src/pages/Home.js
@@ -1,7 +1,32 @@
+import { useEffect } from "react";
+import { useState } from 'react';
+import Announcement from "./layout/home/Announcement";
+
 const Home = () => {
+    const [announcements, setAnnouncemens] = useState([])
+    const [isDataReady, setIsDataReady] = useState(false)
+
+    useEffect(() => {
+        const fetchAnnouncements = async () => {
+            const res = await fetch('api/Announcements');
+            const data = await res.json();
+            setAnnouncemens(data);
+            setIsDataReady(true);
+        }
+        fetchAnnouncements();
+    }, [])
+
     return (
         <div style={{height: "150vh"}}>
-
+            {isDataReady
+                ?   <div style={{display: 'flex', flexWrap: 'wrap', marginTop: '100px'}}>
+                        {announcements.map((announcement) => {
+                            return(
+                            <Announcement key={announcement.id} announcement={announcement} />
+                            )
+                        })}
+                    </div>
+                : <></>}
         </div>
     );
 };

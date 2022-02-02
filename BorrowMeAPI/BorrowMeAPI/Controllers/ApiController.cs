ea956e58a9945399a17a469d7b699791ce16a59b
Pola Jedrecka <pola.jedrecka5@gmail.com>
2022-02-02 12:03:22 +0100

    API_Controller: Add all endpoints without implementation

--- BorrowMeAPI/BorrowMeAPI/Controllers/ApiController.cs
+++ BorrowMeAPI/BorrowMeAPI/Controllers/ApiController.cs
@@ -1,5 +1,6 @@
 ﻿using BorrowMeAPI.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
 
 
 namespace BorrowMeAPI.Controllers
@@ -8,8 +9,7 @@ namespace BorrowMeAPI.Controllers
     [ApiController]
     public class ApiController : ControllerBase
     {
-        [Route("/api/Announcements")]
-        [HttpGet]
+        [HttpGet("Announcements")]
         public IActionResult GetAllAnnouncements()
         {
             User user = new User()
@@ -73,23 +73,82 @@ namespace BorrowMeAPI.Controllers
             //return Ok(user);
             return Ok(new List<Announcement>() { mock1, mock2, mock3});
         }
-
-
-
+        
         // /api/Announcements POST
+        [HttpPost("Announcements")]
+        public async Task<IActionResult> AddNewAnnouncement()
+        {
+            return Ok();
+        }
 
         // /api/Announcements/{id} GET
+        [HttpGet("Announcements/{id:int}")]
+        public async Task<IActionResult> GetAnnouncementById(int id)
+        {
+            return Ok();
+        }
+
         // /api/Announcements/{id} PUT
+        [HttpPut("Announcements/{id:int}")]
+        public async Task<IActionResult> EditWholeAnnouncement(int id)
+        {
+            return Ok();
+        }
+        
         // /api/Announcements/{id} PATCH
+        [HttpPatch("Announcements/{id:int}")]
+        public IActionResult EditOneInAnnouncement(int id)
+        {
+            return Ok();
+        }
+
         // /api/Announcements/{id} DELETE
-        // /api/Announcements GET
-        // /api/Announcements/{category}/{voivodship}/{city}/{search_phrase} GET
+        [HttpDelete("Announcements/{id:int}")]
+        public async Task<IActionResult> DeleteAnnouncement(int id)
+        {
+            return Ok();
+        }
+
+        // /api/Announcements/{category}/{voivodeship}/{city}/{search_phrase} GET
+        [HttpGet("Announcements/{category}/{voivodeship}/{city}/{searchPhrase}")]
+        public async Task<IActionResult> GetAnnouncementByFilters(string category, string voivodeship, string city, string searchPhrase)
+        {
+            return Ok();
+        }
 
         // /api/Announcements/{id}/Reservation POST
+        [HttpPost("Announcements/{id}/Reservation")]
+        public async Task<IActionResult> AddNewReservation(int id)
+        {
+            return Ok();
+        }
+        
         // /api/Announcements/{id}/Reservation DELETE
+        [HttpDelete("Announcements/{id}/Reservation")]
+        public async Task<IActionResult> DeleteReservation(int id)
+        {
+            return Ok();
+        }
 
         // /api/Notifications POST
+        [HttpPost("Notifications")]
+        public async Task<IActionResult> AddNewNotification([FromBody] Notification notification)
+        {
+            return Ok();
+        }
+        
         // /api/Users/{user_id}/Notifications GET
+        [HttpGet("Users/{user_id}/Notifications")]
+        public async Task<IActionResult> GetAllUserNotifications(int userId)
+        {
+            return Ok();
+        }
+        
         // /api/Users/{user_id}/Notifications/{id} DELETE
+        [HttpDelete("Users/{user_id}/Notifications")]
+        public async Task<IActionResult> DeleteUserNotification(int userId)
+        {
+            return Ok();
+        }
     }
 }

0cacabbc87769f53822702b1119d352491f139a5
Pola Jedrecka <pola.jedrecka5@gmail.com>
2022-03-14 11:09:02 +0100

    Front-end: Implement get user details from back-end

--- BorrowMeReact/src/pages/UserPage.js
+++ BorrowMeReact/src/pages/UserPage.js
@@ -1,9 +1,25 @@
 import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {getData} from "../services/apiFetch";
 
 const UserPage = () => {
+    const [userDetails, setDetails] = useState();
     const userId = useParams()
+    useEffect(() => {
+            let response = getData(`/api/users/${userId}/details`)
+                .then(data => {
+                    setDetails(data);
+                })
+        }, []
+    )
     return (
-        <p>Tu będzie strona użytkownika z wyświetlonymi wszystkimi jego ogłoszeniami</p>
+        <>
+            <p>Tu będzie strona użytkownika z wyświetlonymi wszystkimi jego ogłoszeniami</p>
+            {userDetails &&
+                <div>
+                </div>
+            }
+        </>
     )
 }
 

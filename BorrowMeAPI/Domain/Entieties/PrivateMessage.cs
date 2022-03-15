4723d7526d1a23c91f80a2787fa0149a1915fe35
Dominik Berniak <berniak.dominik@gmail.com>
2022-03-15 15:48:33 +0100

    Back-end: Add PrivateMessage entity

---
+++ BorrowMeAPI/Domain/Entieties/PrivateMessage.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entieties
+{
+    public class PrivateMessage
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [Required, StringLength(500)]
+        public string Text { get; set; }
+        [Required]
+        public User Sender { get; set; }
+        [Required]
+        public User Receiver { get; set; }
+        public DateTime SendTime { get; set; } = DateTime.Now;
+        [Required]
+        public bool IsRead { get; set; } = false;
+    }
+}

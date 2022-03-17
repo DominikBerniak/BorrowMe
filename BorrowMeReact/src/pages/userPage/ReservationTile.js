c69339f535027938a4b91142052c967ecaa94523
Pola Jedrecka <pola.jedrecka5@gmail.com>
2022-03-17 21:55:58 +0100

    Front-end: Add handle accept body function to accept reservation button

--- BorrowMeReact/src/pages/userPage/ReservationTile.js
+++ BorrowMeReact/src/pages/userPage/ReservationTile.js
@@ -5,15 +5,22 @@ import DoneIcon from "../../components/DoneIcon";
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import MessageIcon from "../../components/MessageIcon";
 import StarIcon from "../../components/StarIcon";
+import {patchData} from "../../services/apiFetch";
 
-const ReservationTile = ({reservation, classNames, isAccepted = true, isExpired = false, isUserReservation = true}) => {
+const ReservationTile = ({reservation, classNames, isAccepted = true, isExpired = false, isUserReservation = true, state, setDummyState}) => {
     const startDay = new Date(reservation.reservationStartDay);
     const endDay = new Date(reservation.reservationEndDay);
     const navigate = useNavigate();
     let handleClick = () => {
         navigate(`/announcement/${reservation.announcement.id}`)
     }
-    let deleteReservation = () => {
+    let handleDeleteReservation = () => {
+    }
+    let handleAccept = () => {
+        let response = patchData(`/api/Reservations/${reservation.id}/accept`, true)
+            .then(response => {
+            })
+        setDummyState(!state)
     }
 
     return (
@@ -28,12 +35,12 @@ const ReservationTile = ({reservation, classNames, isAccepted = true, isExpired 
             </div>
             <div className="delete-reservation-button">
                 {!isAccepted && !isExpired && !isUserReservation &&
-                    <button type="button" className="btn btn-success btn-sm accept-button">Akceptuj <DoneIcon/></button>}
+                    <button type="button" className="btn btn-success btn-sm accept-button" onClick={handleAccept}>Akceptuj <DoneIcon/></button>}
                 {isAccepted && !isExpired && isUserReservation &&
                     <button type="button" className="btn btn-sm contact-button">Skontaktuj się <MessageIcon/></button>}
                 {isExpired &&
                     <button type="button" className="btn btn-info btn-sm rate-button"><div className="me-1">Oceń</div> <StarIcon/></button>}
-                <button type="button" className="btn delete-button" onClick={deleteReservation}><XIcon/></button>
+                <button type="button" className="btn delete-button" onClick={handleDeleteReservation}><XIcon/></button>
             </div>
         </div>
     )
